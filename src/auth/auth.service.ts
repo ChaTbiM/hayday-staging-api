f5c7991cc5819a5a26f78c5862f9fc5c15639e30
mustapha chatbi <chatbimdev@gmail.com>
2021-08-07 19:06:18 +0100

    included user in token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -33,7 +33,7 @@ export class AuthService {
   }
 
   async login(user) {
-    const payload = { email: user.email, sub: user.id, role: user.role };
+    const payload = { email: user.email, sub: user.id, user , role:user.role };
     return {
       access_token: this.jwtService.sign(payload),
       user,
